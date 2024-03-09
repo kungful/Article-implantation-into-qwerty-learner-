cb0ed2d2d4e774d7318e59631463a62ef5f453e1
hua(Kungfu) <84950880+kungful@users.noreply.github.com>
2024-03-09 09:53:05 +0800

    Update win_python_rum.bat
    使用 %~dp0 来获取脚本所在的目录，并构建路径。这样无论脚本被放置在哪个位置，它都会正确地找到它需要的文件。以下是修改后的脚本：

--- win_python_rum.bat
+++ win_python_rum.bat
@@ -1,17 +1,27 @@
 @echo off
-REM 检查是否存在虚拟环境，如果不存在则创建
-if not exist .\myenv\Scripts\activate.bat (
-    python -m venv myenv
+
+REM 获取脚本所在目录
+set script_dir=%~dp0
+
+REM 定义虚拟环境目录
+set venv_dir=%script_dir%myenv
+
+REM 切换到脚本所在目录
+cd /d %script_dir%
+
+REM 检查是否存在虚拟环境
+if exist %venv_dir%\Scripts\activate.bat (
+    REM 如果存在，则激活虚拟环境并运行脚本
+    call %venv_dir%\Scripts\activate
+    python %script_dir%1demo.py
+    REM 关闭虚拟环境
+    deactivate
+) else (
+    REM 如果不存在，则创建虚拟环境，并运行脚本
+    python -m venv %venv_dir%
+    call %venv_dir%\Scripts\activate
+    pip install -r %script_dir%requirements.txt
+    python %script_dir%1demo.py
+    REM 关闭虚拟环境
+    deactivate
 )
-
-REM 激活虚拟环境
-call .\myenv\Scripts\activate
-
-REM 安装 requirements.txt 中列出的依赖项
-pip install -r requirements.txt
-
-REM 运行 Python 脚本
-python 1demo.py
-
-REM 关闭虚拟环境
-deactivate
